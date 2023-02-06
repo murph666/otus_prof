6d89a358047cb09c05899d74efc0cc3ccf951845
murph666 <76002710+murph666@users.noreply.github.com>
2023-02-07 00:28:06 +0300

    asd
    Чтение из файла в ip_pool

--- 02_homework/ip_filter.cpp
+++ 02_homework/ip_filter.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <dirent.h>
+#include <unistd.h>
+#include <fstream>
+#include <sys/stat.h>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -16,7 +21,7 @@ std::vector<std::string> split(const std::string &str, char d)
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
+    while (stop != std::string::npos)
     {
         r.push_back(str.substr(start, stop - start));
 
@@ -31,99 +36,75 @@ std::vector<std::string> split(const std::string &str, char d)
 
 int main(int argc, char const *argv[])
 {
-    try
-    {
-        std::vector<std::vector<std::string> > ip_pool;
+    char cwd[1024];
+    std::map<int, std::string> fileboard;
+    std::ifstream file;
 
-        for(std::string line; std::getline(std::cin, line);)
+    if (getcwd(cwd, 1024) != NULL)
+    {
+        std::cout << "Current working directory: " << cwd << std::endl;
+    }
+    else
+    {
+        std::cout << "Error getting current directory!" << std::endl;
+    }
+
+    if (argc == 1)
+    {
+        DIR *dirp;
+        struct dirent *dp;
+        struct stat info;
+        short num = 0;
+
+        dirp = opendir(cwd);
+
+        while ((dp = readdir(dirp)) != NULL)
+        {
+            if (stat(dp->d_name, &info) == 0 && !S_ISDIR(info.st_mode))
+            {
+                std::string filename = dp->d_name;
+                std::string extension = filename.substr(filename.rfind(".") + 1);
+                if (extension == "tsv")
+                {
+                    fileboard.insert(std::pair<int, std::string>(num, dp->d_name));
+                    num++;
+                }
+            }
+        }
+        closedir(dirp);
+
+        std::cout << "List of files: " << std::endl;
+        for (const auto &v : fileboard)
+            std::cout << v.first << ". " << v.second << std::endl;
+
+        int f;
+        std::cout << "Enter the file number: ";
+        std::cin >> f;
+        file = std::ifstream(fileboard.at(f));
+    }
+    else if (argc == 2)
+    {
+        std::string filename = argv[1];
+        std::string extension = filename.substr(filename.rfind(".") + 1);
+        if (extension == "tsv") file = std::ifstream(filename);
+        else std::cout << "Invalid file format";
+    }
+    else
+    {
+        std::cout << "Invalid command";
+    }
+
+    std::string line;
+    std::vector<std::vector<std::string>> ip_pool;
+    if (file.is_open())
+    {
+        for (std::string line; std::getline(file, line);)
         {
             std::vector<std::string> v = split(line, '\t');
             ip_pool.push_back(split(v.at(0), '.'));
         }
 
-        // TODO reverse lexicographically sort
-
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        file.close();
     }
-    catch(const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-
     return 0;
 }
