dba49bff51f82d1bb52ae1259618aa87134b9121
murph666 <76002710+murph666@users.noreply.github.com>
2023-05-26 18:30:05 +0300

    init

---
+++ 06_Homework/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <tuple>
+
+int main (int, char **) {
+
+    return 0;
+}
---
+++ 06_Homework/matrix.h
@@ -0,0 +1,9 @@
+//
+// Created by murph on 22.04.23.
+//
+#include <iostream>
+#include <vector>
+#include <list>
+#include <tuple>
+#define UNUSED(x) (void)x;
+
