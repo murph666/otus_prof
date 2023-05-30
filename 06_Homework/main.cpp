540170dc74732329d4164ae17a966a24c36c254c
murph666 <76002710+murph666@users.noreply.github.com>
2023-05-30 15:29:36 +0300

    matrix done

--- 06_Homework/main.cpp
+++ 06_Homework/main.cpp
@@ -6,13 +6,13 @@
 int main (int, char **) {
 
     Matrix<int, -1> matrix;
-//    assert(matrix.size() == 0); // все ячейки свободны
-//    auto a = matrix[0][0];
-//    assert(a == -1);
-//    assert(matrix.size() == 0);
-//    matrix[100][100] = 314;
-//    assert(matrix[100][100] == 314);
-//    assert(matrix.size() == 1);
+    assert(matrix.size() == 0); // все ячейки свободны
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
     // выведется одна строка
     // 100100314
     for(auto &c: matrix)
