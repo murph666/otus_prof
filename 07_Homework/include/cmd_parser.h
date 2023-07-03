8f738508cf6e44d266bf8d52f4b67415d1c2a7b2
murph666 <76002710+murph666@users.noreply.github.com>
2023-07-04 00:12:50 +0300

    fix 7hm

---
+++ 07_Homework/include/cmd_parser.h
@@ -0,0 +1,52 @@
+//
+// Created by murph on 26.06.23.
+//
+
+#ifndef INC_07_HOMEWORK_CMD_PARSER_H
+#define INC_07_HOMEWORK_CMD_PARSER_H
+
+#include <string>
+#include <stack>
+#include <array>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class cmd_parser {
+public:
+    explicit cmd_parser(std::stack<std::string> *arg_batch, const size_t arg_size):m_cmdBatch(arg_batch), m_batch_size(arg_size){};
+
+    void exec();
+
+private:
+    enum _command_literal_types : size_t {
+        CMD_LT_DYNAMIC_BLOCK_BEGIN = 0,
+        CMD_LT_DYNAMIC_BLOCK_END,
+        CMD_LT_END_OF_FILE,
+
+        CMD_LT_COUNT
+    };
+
+    static constexpr std::array<std::string_view, CMD_LT_COUNT> _command_literals = {
+            "{",
+            "}",
+            "EOF"
+    };
+
+    static void print_result(const std::vector<std::string>& node) {
+        std::cout << "bulk:";
+        for (auto& i: node) {
+            std::cout << (i == *node.begin() ? " " : ", ") << i;
+        }
+        std::cout << std::endl;
+    }
+
+    std::stack<std::string> *m_cmdBatch;
+    size_t m_batch_size;
+    std::stack<std::vector<std::string>> m_command_stack;
+    std::vector<std::string> m_result;
+};
+
+
+#endif //INC_07_HOMEWORK_CMD_PARSER_H
---
+++ 07_Homework/source/cmd_parser.cpp
@@ -0,0 +1,71 @@
+//
+// Created by murph on 26.06.23.
+//
+
+#include "cmd_parser.h"
+
+void cmd_parser::exec() {
+    if (m_command_stack.empty()) {
+        m_command_stack.emplace();
+    }
+
+    auto temp_cmd = m_cmdBatch->top();
+
+    auto it = std::find_if(_command_literals.begin(), _command_literals.end(),[=](
+            std::basic_string_view<char> i) { return i == temp_cmd;});
+
+    if (it == _command_literals.end()) {
+        /* add command to current stack node and pop stack if size > 1 or return result if full */
+        auto& cur = m_command_stack.top();
+        cur.emplace_back(temp_cmd);
+
+        if (cur.size() == m_batch_size) {
+            if (m_command_stack.size() > 1) {
+                m_result.insert(m_result.begin(), cur.begin(), cur.end());
+                m_command_stack.pop();
+            } else if (m_command_stack.size() == 1) {
+                print_result(cur);
+                m_result.clear();
+                cur.clear();
+            }
+        }
+    }
+
+    switch (it - _command_literals.begin()) {
+        case CMD_LT_DYNAMIC_BLOCK_BEGIN: { /* return last result and push stack */
+            auto& cur = m_command_stack.top();
+            if (m_command_stack.size() == 1 && !cur.empty()) {
+                print_result(cur);
+                cur.clear();
+            }
+            m_command_stack.emplace();
+            break;
+        }
+        case CMD_LT_DYNAMIC_BLOCK_END: { /* pop stack or return result if stack with 1 element */
+            if (m_command_stack.size() > 1) {
+                auto& cur = m_command_stack.top();
+                m_result.insert(m_result.end(), cur.begin(), cur.end());
+                m_command_stack.pop();
+            }
+            if (m_command_stack.size() == 1) {
+                print_result(m_result);
+                m_result.clear();
+                m_command_stack.top().clear();
+            }
+            break;
+        }
+        case CMD_LT_END_OF_FILE: { /* return result if stack with 1 element or pop stack */
+            if (m_command_stack.size() > 1) {
+                m_command_stack.pop();
+            } else if (m_command_stack.size() == 1) {
+                auto& cur = m_command_stack.top();
+                m_result.insert(m_result.end(), cur.begin(), cur.end());
+                print_result(m_result);
+                m_result.clear();
+                cur.clear();
+            }
+            break;
+        }
+    }
+}
+
