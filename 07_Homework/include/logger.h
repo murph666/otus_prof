ac80c1b063c0c28250fd68683cdd8bfd37b0a605
murph666 <76002710+murph666@users.noreply.github.com>
2023-07-04 17:39:38 +0300

    add logger

--- 07_Homework/include/logger.h
+++ 07_Homework/include/logger.h
@@ -19,9 +19,6 @@ public:
     Logger() {
         std::filesystem::path directoryPath("log");
         std::filesystem::create_directory(directoryPath);
-        std::string currentTimeFormatted = getCurrentTimeFormatted();
-        std::cout << "Текущее время в формате HHmmddMMYYYY: " << currentTimeFormatted << std::endl;
-//        m_logfile.open(m_filename, std::ios::app);
     }
 
     ~Logger() {
@@ -38,22 +35,25 @@ public:
     }
 
     void writeLog(const std::vector<std::string>& messages) {
-        if (m_logfile.is_open()) {
-            for (const auto& message : messages) {
-                m_logfile << message << std::endl;
-            }
+        std::string currentTimeFormatted = getCurrentTimeFormatted();
+        std::cout << "Текущее время в формате HHmmssddMMYYYY: " << currentTimeFormatted << std::endl;
+        m_filename = std::string("log/log") + currentTimeFormatted;
+        m_logfile.open(m_filename, std::ios::app);
+        for (const auto& message : messages) {
+            m_logfile << message << std::endl;
         }
+        m_logfile.close();
     }
 
     static std::string getCurrentTimeFormatted() {
         auto currentTime = std::chrono::system_clock::now(); // Получение текущего времени
-        auto roundedTime = std::chrono::time_point_cast<std::chrono::minutes>(currentTime); // Округление времени до минут
+        auto roundedTime = std::chrono::time_point_cast<std::chrono::seconds>(currentTime); // Округление времени до минут
 
         // Преобразование времени в строку с нужным форматом
         std::time_t currentTime_t = std::chrono::system_clock::to_time_t(roundedTime);
         std::tm* currentTime_tm = std::localtime(&currentTime_t);
         std::ostringstream oss;
-        oss << std::put_time(currentTime_tm, "%H%M%d%m%Y");
+        oss << std::put_time(currentTime_tm, "%H%M%s%d%m%Y");
 
         return oss.str();
     }
